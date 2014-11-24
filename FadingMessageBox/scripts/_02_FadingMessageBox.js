7d0e010dfe073ae90b37a085a18f32973f6d2625
MeLean <milen820430@gmail.com>
2014-11-24 19:24:51 +0200

    Task Two

---
+++ FadingMessageBox/scripts/_02_FadingMessageBox.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+    $.fn.FadingMessage = function () {
+        $(document).on('click', '#tryMe', function () {
+            var FADE_TIME = 3000;
+            var $value = $('#ErrorMsg').val();
+            var $div = $('<div class="fadeBox"></div>').text($value);
+            $('body').append($div).css('color','red').append('<br>');
+            $('.fadeBox').fadeOut(FADE_TIME);
+        });
+    }
+})(jQuery);
+
+$('#tryMe').FadingMessage();
